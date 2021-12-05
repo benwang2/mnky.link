ed376b6f352e0fd25bedbcd599044d0878aa4c84
benwang2 <eb.benwang@gmail.com>
2021-12-05 18:01:22 -0500

    Removed functionality for making requests to validate URLs and added more monkey related emojis.

--- shorten.js
+++ shorten.js
@@ -17,6 +17,7 @@ var axios = require('axios')
 function exists(GUID){
     return database.ref().child("monkeys").child(GUID).get().then(function(snapshot){
         try {
+            console.log(snapshot.exists(),snapshot.val())
             return snapshot.exists() ? snapshot.val() : false;
         } catch (e) {
             console.error(e);
@@ -30,25 +31,12 @@ function exists(GUID){
   source: https://gist.github.com/dperini/729294
 */
 let urlRegEx = new RegExp(/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i);
-let characters = "🙊,🙉,🙈,🐒,🐵,🍌,💩​​".split(",")
+let characters = "🙊,🙉,🙈,🐒,🐵,🍌,💩,🍇,🍎,🍏,🥕,​​,🥥,🌴,🏝️,🌲".split(",")
 
 async function getValidUrl(url){
     if (url.trim()=="") return false;
     if (!url.match(urlRegEx)) return false;
-    try {
-        if (url.substring(0,6)!="https:" && url.substring(0,5)!="http:") {
-            let [test1, test2, validUrl] = ["https://" + url, "http://" + url, false];
-            let [res1, res2]  = [await axios.get(test1), await axios.get(test2)]
-            validUrl = (res1 != null ? test1 : (res2 != null ? test2 : false));
-        } else {
-            let res1 = await axios.get(url)
-            validUrl = (res1 != null ? url : false)
-        }
-    } catch (e) {
-
-        validUrl = e.request.finished ? e.request._redirectable._currentUrl : false
-    }
-    return validUrl
+    return url
 }
 
 async function genUID(){
